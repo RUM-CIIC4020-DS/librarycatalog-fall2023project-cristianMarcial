8092e8698731219d1eae2d098165bb807e3b6912
Cristian Marcial <cristian.marcial@upr.edu>
2023-10-08 01:27:14 -0400

    fixed some changes

--- src/main/User.java
+++ src/main/User.java
@@ -3,20 +3,24 @@ package main;
 import interfaces.List;
 
 public class User {
+	int id;
+	String name;
+	List<Book> books;
+	
 	public int getId() {
-		return -25;
+		return id;
 	}
 
 	public void setId(int id) {
-		
+		this.id = id;
 	}
 
 	public String getName() {
-		return "";
+		return name;
 	}
 
 	public void setName(String name) {
-		
+		this.name = name;
 	}
 
 	public List<Book> getCheckedOutList() {
