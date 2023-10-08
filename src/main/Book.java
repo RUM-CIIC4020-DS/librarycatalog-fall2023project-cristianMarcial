f0e96af8224053a464f282c44dd3dd80aea67787
Cristian Marcial <cristian.marcial@upr.edu>
2023-10-08 15:06:46 -0400

    added everything needed.

--- src/main/Book.java
+++ src/main/Book.java
@@ -5,11 +5,12 @@ import java.time.temporal.ChronoUnit;
 
 public class Book {
 	//Information about each single book:
-	int id;
-	String title, author, genre;
-	LocalDate lastCheckOutDate;
-	boolean checkedOut;
+	private int id;
+	private String title, author, genre;
+	private LocalDate lastCheckOutDate;
+	private boolean checkedOut;
 	
+	//Constructor
 	public Book(int id, String title, String author, String genre, LocalDate lastCheckOutDate, boolean checkedOut) {
 		this.id = id;
 		this.title = title;
@@ -66,11 +67,11 @@ public class Book {
 	}
 	public float calculateFees() {
 		//According the rubric today's date is 2023/15/8 + 31 Days (23/15/9).
-		LocalDate from31Days = LocalDate.of(2023, 8, 15); 
-		long daysPassed = lastCheckOutDate.until(from31Days, ChronoUnit.DAYS);
+		LocalDate from31Days = LocalDate.of(2023, 9, 15); 
+		long daysPassed = lastCheckOutDate.until(LocalDate.of(2023, 9, 15), ChronoUnit.DAYS);
 		
 		/* fee (if applicable) = base fee + 1.5 per additional day */
-		if(lastCheckOutDate.isAfter(from31Days)) 
+		if(lastCheckOutDate.isBefore(LocalDate.of(2023, 8, 16))) 
 			return (float) (10 + 1.5 * ((int)(daysPassed) - 31));
 		return 0.0f;
 	}
