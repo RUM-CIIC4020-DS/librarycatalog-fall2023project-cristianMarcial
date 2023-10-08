9c98c7c450a280f341011fdd17f88b4824c5fcfe
Cristian Marcial <cristian.marcial@upr.edu>
2023-10-07 23:18:25 -0400

    added functions

--- src/main/Book.java
+++ src/main/Book.java
@@ -1,61 +1,68 @@
 package main;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Book {
+	//Information about each single book:
+	int id;
+	String title, author, genre;
+	LocalDate lastCheckOutDate;
+	boolean checkedOut;
 	
+	//Getters and Setters functions
 	public int getId() {
-		return -10;
+		return id;
 	}
 	public void setId(int id) {
-		
+		this.id = id;
 	}
 	public String getTitle() {
-		return "";
+		return title;
 	}
 	public void setTitle(String title) {
-		
+		this.title = title;
 	}
 	public String getAuthor() {
-		return "";
+		return author;
 	}
 	public void setAuthor(String author) {
-		
+		this.author = author;
 	}
 	public String getGenre() {
-		return "";
+		return genre;
 	}
 	public void setGenre(String genre) {
-		
+		this.genre = genre;
 	}
 	public LocalDate getLastCheckOut() {
-		return null;
+		return lastCheckOutDate;
 	}
 	public void setLastCheckOut(LocalDate lastCheckOut) {
-		
+		this.lastCheckOutDate = lastCheckOut;
 	}
 	public boolean isCheckedOut() {
-		return false;
+		return checkedOut;
 	}
 	public void setCheckedOut(boolean checkedOut) {
-		
+		this.checkedOut = checkedOut;
 	}
 	
 	@Override
 	public String toString() {
-		/*
-		 * This is supposed to follow the format
-		 * 
+		/* This is supposed to follow the format
 		 * {TITLE} By {AUTHOR}
-		 * 
-		 * Both the title and author are in uppercase.
-		 */
-		return "";
+		 * Both the title and author are in uppercase. */
+		return title.toUpperCase() + " BY " + author.toUpperCase() + "\n";
 	}
 	public float calculateFees() {
-		/*
-		 * fee (if applicable) = base fee + 1.5 per additional day
-		 */
-		return -1000;
+		//According the rubric today's date is 2023/15/8 + 31 Days (23/15/9).
+		LocalDate from31Days = LocalDate.of(2023, 8, 15); 
+		long daysPassed = lastCheckOutDate.until(from31Days, ChronoUnit.DAYS);
+		
+		/* fee (if applicable) = base fee + 1.5 per additional day */
+		if(lastCheckOutDate.isAfter(from31Days)) 
+			return (float) (10 + 1.5 * ((int)(daysPassed) - 31));
+		return 0.0f;
 	}
 }
